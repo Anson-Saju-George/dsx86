0e23af97fe42085220621cb48d7143999958fde8
Anson Saju George <ansonsaju@karunya.edu.in>
2025-12-07 22:48:21 +0530

    dsa: grind - I

--- 005_binary_search.py
+++ 005_binary_search.py
@@ -3,7 +3,7 @@ def binary_search(nums, target):
     right = len(nums) - 1 # Initialize right pointer
 
     while left <= right: # Continue searching while the left pointer is less than or equal to the right pointer
-        mid = (left + right) // 2 # Calculate the middle index
+        mid = (left + right) // 2 # Calculate the middle index by taking the floor of the average of left and right
 
         if nums[mid] == target: # Check if the middle element is the target
             return mid # Target found, return the index
@@ -18,5 +18,6 @@ def binary_search(nums, target):
 
 
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(binary_search(nums, 5))  # Output: 4
-print(binary_search(nums, 11)) # Output: -1
+print(binary_search(nums, 2))  # Output: 1
+# print(binary_search(nums, 11)) # Output: -1
+
---
+++ 006_divisions.md
@@ -0,0 +1,10 @@
+```
+# Divisions in Python
+
+| Operator      | Meaning             |
+| ------------- | ------------------- |
+| /             | Division (float)    |
+| //            | Floor division      |
+| %             | Remainder           |
+| divmod(a,b)   | Gives  (a//b, a%b)  |
+```
