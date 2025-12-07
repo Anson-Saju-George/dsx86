61bc8e99407abb616e95e51e5fe3206e37e3377b
Anson Saju George <ansonsaju@karunya.edu.in>
2025-12-07 12:16:16 +0530

    dsa: grind - I

--- run.py
+++ run.py
@@ -11,3 +11,20 @@ nums = [1, 2, 3, 4, 5]
 target = 9
 print(two_sum(nums, target))
 
+
+def two_sum_multiple(nums, target):
+    Map = {}
+    res = []
+    
+    for i, val in enumerate(nums):
+        comp = target - val
+        if comp in Map:
+            for prev in Map[comp]:
+                if prev in prev:
+                    res.append([prev, i])
+        
+        if val not in Map:
+            Map[val] = []
+        Map[val].append(i)
+    return res if res else None
+
