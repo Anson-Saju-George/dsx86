58381ec2a1d086a9be9b28ea6090e91ba055d604
Anson Saju George <ansonsaju@karunya.edu.in>
2025-12-11 16:52:09 +0530

    dsa: grind

--- Phase 1/016_Peak.py
+++ Phase 1/016_Peak.py
@@ -20,9 +20,28 @@ def find_peak(nums):
     # lo == hi is a peak index
     return lo
 
-nums = [1, 2, 3, 1]
+
+def find_all_peaks(nums):
+    n = len(nums)
+    peaks = []
+
+    for i in range(n):
+        left  = nums[i-1] if i > 0 else float('-inf')
+        right = nums[i+1] if i < n-1 else float('-inf')
+
+        if nums[i] > left and nums[i] > right:
+            peaks.append(i)
+
+    return peaks
+
+
+# Example usage:
+nums = [1, 5, 3, 4, 1]
 print(find_peak(nums))
 nums = [0, 1, 0, 2, 1]
 print(find_peak(nums))
 nums = [3, 2, 1] + [1, 2, 3,]
 print(find_peak(nums))
+
+nums = [1, 5, 3, 4, 1, 7, 6]
+print(find_all_peaks(nums))
